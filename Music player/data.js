4fd72672a9c5290cedf85d110f12a8a6b1351b09
Ejim Joseph <43706906+jossy29@users.noreply.github.com>
2024-02-09 20:52:03 +0100

    Add files via upload

---
+++ Music player/data.js
@@ -0,0 +1,31 @@
+let songs = [{
+        name: 'song 1',
+        path: 'musics/Song 1.m4a',
+        artist: 'artist 1',
+        cover: 'images/cover 1.png'
+    },
+    {
+        name: 'song 2',
+        path: 'musics/Song 2.m4a',
+        artist: 'artist 2',
+        cover: 'images/cover 2.png'
+    },
+    {
+        name: 'song 3',
+        path: 'musics/Song 3.m4a',
+        artist: 'artist 3',
+        cover: 'images/cover 3.png'
+    },
+    {
+        name: 'song 4',
+        path: 'musics/Song 4.m4a',
+        artist: 'artist 4',
+        cover: 'images/cover 4.png'
+    },
+    {
+        name: 'song 5',
+        path: 'musics/Song 5.m4a',
+        artist: 'artist 5',
+        cover: 'images/cover 5.png'
+    },
+]
